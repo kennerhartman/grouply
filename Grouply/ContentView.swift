5a2d606268e65956b17f77c0b6f5c47475c749d0
kennerhartman <90115782+kennerhartman@users.noreply.github.com>
2026-07-21 13:01:08 -0600

    Initial Commit

---
+++ Grouply/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Grouply
+//
+//  Created by Kenner Hartman on 7/21/26.
+//  Copyright © 2026 Kenner Hartman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grouply/GrouplyApp.swift
@@ -0,0 +1,18 @@
+//
+//  GrouplyApp.swift
+//  Grouply
+//
+//  Created by Kenner Hartman on 7/21/26.
+//  Copyright © 2026 Kenner Hartman. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GrouplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
